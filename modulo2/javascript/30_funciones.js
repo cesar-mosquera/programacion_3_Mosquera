e07afa974cfe9f33e0fe833218171ad501b22381
cesar-mosquera <juliomosquera252@gmail.com>
2026-05-04 09:56:10 -0500

    CLASE 04-05-2026

---
+++ modulo2/javascript/30_funciones.js
@@ -0,0 +1,36 @@
+// Funcion declarada
+function saludo(){
+    console.log("Hello World");
+}
+
+saludo();
+
+//Funcion Expresada
+const saludarHola = function(){
+    console.log("Hola con funcion expresada");
+}
+saludarHola();
+
+// Funcion Flecha
+const saludosFlecha=()=>{
+    console.log("Hello con Funcion Flecha");
+}
+saludosFlecha();
+
+//Funcion Anonima
+setTimeout(function(){
+    console.log("Ejecutando...");
+},1000)
+
+// Funcion con parametros
+function saludarConParametros(nombre){
+    console.log("Hola "+nombre)
+}
+saludarConParametros("Pedro");
+
+// Sintaxis: function nombre(parámetros) { cuerpo }
+function saludar(nombre) {
+    return `Hola, ${nombre}!`;
+  }
+  console.log(saludar("Ana"));   // "Hola, Ana!"
+  console.log(saludar("Luis"));  // "Hola, Luis!"
