9ceede3c2de02210d0cffe7d577b822d51dc9ed9
cesar-mosquera <juliomosquera252@gmail.com>
2026-05-07 09:01:42 -0500

    CLASE 07-05-2026

--- modulo2/javascript/32_arrays.js
+++ modulo2/javascript/32_arrays.js
@@ -167,3 +167,23 @@ const anidado = [[1, 2], [3, 4], [5, 6]];
 const plano   = anidado.reduce((acum, arr) => [...acum, ...arr], []);
 console.log(plano);   // [1, 2, 3, 4, 5, 6]
 // alternativa moderna: anidado.flat()
+
+// EJERCICIOS EN CLASE:
+/* Usa map para convertir todas a Fahrenheit. Fórmula: (c * 9/5) + 32
+Usa filter para quedarte solo con las temperaturas Celsius entre 0 y 30
+Encadena filter + map: filtra las que estén entre 0 y 30 y conviértelas a Fahrenheit */
+
+const celsius = [0, 15, -5, 22, 37, 100, -10, 28];   
+
+const celsiusFilter = [0, 15, -5, 22, 37, 100, -10, 28];
+
+// Solo los pares
+const celsiusaF = celsius.map(c => (c / 9/5)+32);
+console.log(`Fahrenheit:`,celsiusaF);
+
+const celsius30 = celsiusFilter.filter(n => n >= 0 && n <= 30);
+console.log(`Entre 0 y 30°C:`,celsius30); 
+
+const Fahrenheit = celsiusaF.filter(n => n >= 0 && n <= 30);
+const Encadenamiento = Fahrenheit;
+console.log(`Entre 0 y 30°C en °F:`,Encadenamiento); 
