8ba2bc638c6b07527bf47ba1c21da63e84dee9cb
cesar-mosquera <juliomosquera252@gmail.com>
2026-05-21 09:00:11 -0500

    CLASE 21-05-2026

--- modulo3/blog-backend/src/users/dto/create-user.dto.ts
+++ modulo3/blog-backend/src/users/dto/create-user.dto.ts
@@ -1,13 +1,14 @@
-import { IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, isString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
   username?: string;
 
   @IsString()
-  password?: string;
-
-  @IsString()
+  @IsEmail()
   email?: string;
 
+  @IsString()
+  @MinLength(6)
+  password?: string;
 }
