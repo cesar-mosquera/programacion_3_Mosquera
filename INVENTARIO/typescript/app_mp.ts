0846ad2587a50bed3af280806fa4ccde03f25455
cesar-mosquera <juliomosquera252@gmail.com>
2026-06-25 23:00:00 -0500

    feat(ts): definicion de interfaces para estructura de datos de Productos

--- INVENTARIO/typescript/app_mp.ts
+++ INVENTARIO/typescript/app_mp.ts
@@ -1,3 +1,10 @@
 ﻿// ConfiguraciÃ³n inicial de TypeScript
 const sistema: string = "Inventario Pro";
 console.log(\ cargado con TypeScript.);
+
+interface Producto {
+    id: number;
+    nombre: string;
+    precio: number;
+    stock: number;
+}
